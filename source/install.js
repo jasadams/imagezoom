cdddd208a8ad6311f462a48d9aa83aba87fb9f8a
jason <jason@6f8e2a70-b90e-0410-97f0-afd08a582135>
2006-03-28 18:46:36 +0000

    Update versions to 0.2.6

--- source/install.js
+++ source/install.js
@@ -6,7 +6,7 @@ var XpiInstaller = {
 	// --- Editable items begin ---
 	extFullName: 'Image Zoom', // The name displayed to the user (don't include the version)
 	extShortName: 'imagezoom', // The leafname of the JAR file (without the .jar part)
-	extVersion: '0.2.5.20060313',
+	extVersion: '0.2.6',
 	extAuthor: 'Jason Adams',
 	extLocaleNames: ["en-US","sl-SI","hu-HU","fr-FR","de-DE","zh-TW","ja-JP","cs-CZ","ko-KR","es-ES","es-AR","it-IT","tr-TR"],
 	extSkinNames: ['classic'], // e.g. ['classic', 'modern']
