c5e88ee680d626b36577f121a9d77c1bc2dc850d
jason <jason@6f8e2a70-b90e-0410-97f0-afd08a582135>
2007-12-29 01:58:29 +0000

    Remove faulty sr-YU locale

--- source/install.js
+++ source/install.js
@@ -6,9 +6,9 @@ var XpiInstaller = {
 	// --- Editable items begin ---
 	extFullName: 'Image Zoom', // The name displayed to the user (don't include the version)
 	extShortName: 'imagezoom', // The leafname of the JAR file (without the .jar part)
-	extVersion: '0.3.0.20071227',
+	extVersion: '0.3.0.20071228',
 	extAuthor: 'Jason Adams',
-	extLocaleNames: ["en-US","sl-SI","hu-HU","fr-FR","de-DE","zh-TW","ja-JP","cs-CZ","ko-KR","es-ES","es-AR","it-IT","tr-TR", "zh-CN", "pt-BR", "ru-RU", "mk-MK", "nl-NL", "he-IL", "uk-UA", "ro-RO", "be-BY", "sv-SE", "fi-FI", "hr-HR", "pl-PL", "sk-SK", "sr-YU"],
+	extLocaleNames: ["en-US","sl-SI","hu-HU","fr-FR","de-DE","zh-TW","ja-JP","cs-CZ","ko-KR","es-ES","es-AR","it-IT","tr-TR", "zh-CN", "pt-BR", "ru-RU", "mk-MK", "nl-NL", "he-IL", "uk-UA", "ro-RO", "be-BY", "sv-SE", "fi-FI", "hr-HR", "pl-PL", "sk-SK"],
 	extSkinNames: ['classic'], // e.g. ['classic', 'modern']
 	extPostInstallMessage: null, // Set to null for no post-install message
 	// --- Editable items end ---
